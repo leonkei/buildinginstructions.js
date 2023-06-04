5d22f435471d5e1a4506ef67937705124059b626
leonlam <leon09120912@gmail.com>
2023-06-04 11:25:36 +0800

    refactor: apply to flask deployment

---
+++ server.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask, render_template
+from flask_cors import CORS
+
+app = Flask(__name__, static_folder='static')
+app.debug = True
+
+
+@app.route('/')
+def home():
+    return render_template('sample_view.htm')
+
+
+@app.route('/<path:path>')
+def serve_htm(path):
+    return render_template(path)
+
+
+if __name__ == '__main__':
+    app.run(port=5000)
